a374aa11cb445bbeaa27e7c9e9142d5570d36b52
hjm <hujianming87@gmail.com>
2017-08-14 16:04:25 +0800

    SHMenu

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 
 import logo from './logo.svg';
 
+
 import './App.css';
 import './App.less'
+
+import SHMenu from './components/app/SHMenu'
+
 import { Layout, Menu, Icon } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
+
+//
+// jest.unmock('components/App/SHMenu');
+
 
 // <img src={logo} className="App-logo" alt="logo" />
 class App extends Component {
   render() {
     return (
-
       <Layout style={{ height: "100%" }}>
         <Sider
           breakpoint="lg"
@@ -19,37 +26,20 @@ class App extends Component {
           onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
         >
           <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-              <Menu.Item key="1">
-                <Icon type="user" />
-                <span className="nav-text">nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="video-camera" />
-                <span className="nav-text">nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="upload" />
-                <span className="nav-text">nav 3</span>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Icon type="user" />
-                <span className="nav-text">nav 4</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
+          <SHMenu />
+        </Sider>
         <Layout>
-        <Header style={{ background: '#fff', padding: 0 }} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div style={{ padding: 24, background: '#fff', minHeight: 480 }}>
-            content
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2016 Created by Ant UED
-        </Footer>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 480 }}>
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
     );
   }
 }
